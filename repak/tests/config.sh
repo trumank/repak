fcc868c60e19365fdfa0b9889c8794eff208f6c0
Truman Kilen <t@kilen.me>
2023-02-11 19:23:51 -0600

    Remove accidentally commited config

--- repak/tests/config.sh
+++
@@ -1,6 +0,0 @@
-UNREAL_4_20=/home/truman/scrapbox/ue/4.20/Engine/Binaries/Linux/UnrealPak
-UNREAL_4_21=/home/truman/scrapbox/ue/4.21/Engine/Binaries/Linux/UnrealPak
-UNREAL_4_22=/home/truman/scrapbox/ue/4.22/Engine/Binaries/Linux/UnrealPak
-UNREAL_4_23=/home/truman/scrapbox/ue/4.23/Engine/Binaries/Linux/UnrealPak
-UNREAL_4_25=/home/truman/scrapbox/ue/4.25/Engine/Binaries/Linux/UnrealPak
-UNREAL_4_27=/home/truman/scrapbox/ue/4.27/Engine/Binaries/Linux/UnrealPak
